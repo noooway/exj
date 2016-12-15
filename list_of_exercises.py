c92f2de671c8092726284fffbf9cb48a7714cc03
noway <noway@debian>
2016-12-15 10:03:38 +0300

    Kivy gui pure python

---
+++ list_of_exercises.py
@@ -0,0 +1,62 @@
+class ExerciseDesription( object ):
+    def __init__( self, name, exercise_type ):
+        self.name = name
+        self.exercise_type = exercise_type
+        self.exercise_widget_type = self.exercise_type + 'Widget'
+
+list_of_exercises = []
+
+list_of_exercises.append(
+    ExerciseDesription( 'Push Ups', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Leg Press', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Hack Squat', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Calf Raise', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Deadlift', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Romanian Deadlift', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Barbell Squat', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Lying Leg Curl', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Leg Extension', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Pull Ups', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Lying T-Bar Row', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'T-Bar Row', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Leverage Iso Row', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Machine Bench Press', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Barbell Bench Press', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Leverage Shoulder Press', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Standing Military Press', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Upright Barbell Row', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Triceps Pushdown', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Dips', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Machine Biceps Curl', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Barbell Curl', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Hammer Curl', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Hyperextenstion', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Decline Crunch', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Hanging Leg Raise', 'ExerciseSetsRepsWeights' ) )
+list_of_exercises.append(
+    ExerciseDesription( 'Distance Running', 'ExerciseSetsRepsWeights' ) )
